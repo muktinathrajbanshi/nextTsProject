17f95cf3a9dbb67703f97ff8a11d4a9b474da1ab
unknown <muktinathrajbanshi786@gmail.com>
2026-03-06 09:03:07 +0545

    hait yar balla balla vayo

--- store/todos.tsx
+++ store/todos.tsx
@@ -1,7 +1,21 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export const todosContext = createContext(null);
 
-export const TodosProvider = (children) => {
+export const TodosProvider = (children: {children:ReactNode}) => {
 
+    const [todos, setTodos] = useState();
+
+    const handleAddTodo = (task: string) => {
+
+        setTodos((prev) => {
+            const newTodos = {
+                id: Math.random().toString(),
+                task,
+                complete: false,
+                createdAt: new Date()
+            }
+        })
+
+    }
 }
