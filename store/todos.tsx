9bea4d0240dd60d22e03449f21309a2fb2875132
unknown <muktinathrajbanshi786@gmail.com>
2026-03-06 21:22:03 +0545

    oh my god kati dherai kaam malai

--- store/todos.tsx
+++ store/todos.tsx
@@ -1,5 +1,12 @@
 import { createContext, ReactNode, useState } from "react";
 
+type Todo = {
+    id: string;
+    task:string;
+    completed: boolean;
+    createdAt: Date;
+}
+
 export const todosContext = createContext(null);
 
 export const TodosProvider = (children: {children:ReactNode}) => {
@@ -9,13 +16,15 @@ export const TodosProvider = (children: {children:ReactNode}) => {
     const handleAddTodo = (task: string) => {
 
         setTodos((prev) => {
-            const newTodos = {
+            const newTodos: Todo[] = {
                 id: Math.random().toString(),
                 task,
                 complete: false,
                 createdAt: new Date()
             }
+            ...prev
         })
+        return newTodos;
 
     }
 }
