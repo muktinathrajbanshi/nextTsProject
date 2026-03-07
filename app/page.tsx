b808ab5cb9208da6f69a86d585e2da22bdae551f
unknown <muktinathrajbanshi786@gmail.com>
2026-03-07 21:23:36 +0545

    la hai ta

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import AddTodo from '@/components/add-todo';
+import Todos from '@/components/todos';
 
 const page = () => {
   return (
     <main>
       <h2>TODO NEXT + TYPESCRIPT</h2>
       <AddTodo />
+      <Todos />
     </main>
   );
 };
