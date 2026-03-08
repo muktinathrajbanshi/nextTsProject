fa9c324193af129c231817fe97a846db8b9e1b3f
unknown <muktinathrajbanshi786@gmail.com>
2026-03-08 22:11:32 +0545

    are wah

--- components/todos.tsx
+++ components/todos.tsx
@@ -24,7 +24,7 @@ const Todos = () => {
 
                     {
                         todo.completed && (
-                            <button type="button" onClick={() => handleTodoDelete(todo.id)}></button>
+                            <button type="button" onClick={() => handleTodoDelete(todo.id)}>Delete</button>
                         )
                     }
 
