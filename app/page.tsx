2def6e45695502b914c66616be5c307a6eaa324a
unknown <muktinathrajbanshi786@gmail.com>
2026-03-20 19:06:43 +0545

    necessary dependencies added yar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import AddTodo from '@/components/add-todo';
 import Navbar from '@/components/navbar';
 import Todos from '@/components/todos';
+import "./globals.css";
+import { RiTodoLine } from "react-icons/ri";
+
 
 const page = () => {
   return (
     <main>
-      <h2>TODO NEXT + TYPESCRIPT</h2>
+      <h2><RiTodoLine /> TODO NEXT + TYPESCRIPT <RiTodoLine /></h2>
       <Navbar />
       <AddTodo />
       <Todos />
