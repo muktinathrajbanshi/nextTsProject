4fd0f26acffb00bd6c6a5bcf757792576091b8fe
unknown <muktinathrajbanshi786@gmail.com>
2026-03-22 10:00:04 +0545

    mummy ajai deploy vayena yar aba hunchha hola

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
+import { Suspense } from "react";
 import AddTodo from '@/components/add-todo';
 import Navbar from '@/components/navbar';
 import Todos from '@/components/todos';
 import "./globals.css";
 import { RiTodoLine } from "react-icons/ri";
 
-
 const page = () => {
   return (
     <main>
       <div className="app-container">
-      <h2><RiTodoLine className="icons" /> TODO NEXT + TYPESCRIPT <RiTodoLine className="icons" /></h2>
-      <Navbar />
-      <AddTodo />
-      <Todos />
+
+        <h2>
+          <RiTodoLine className="icons" />
+          TODO NEXT + TYPESCRIPT
+          <RiTodoLine className="icons" />
+        </h2>
+
+        <Suspense fallback={<p>Loading Navbar...</p>}>
+          <Navbar />
+        </Suspense>
+
+        <Suspense fallback={<p>Loading AddTodo...</p>}>
+          <AddTodo />
+        </Suspense>
+
+        <Suspense fallback={<p>Loading Todos...</p>}>
+          <Todos />
+        </Suspense>
+
       </div>
     </main>
   );
